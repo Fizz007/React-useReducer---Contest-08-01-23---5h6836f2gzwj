a712e8112966152ec70d4d5717e47b354ca9ec74
Faisal abrar <114504997+Fizz007@users.noreply.github.com>
2023-01-08 12:45:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,39 @@ const App = () => {
   function onDecrement() {
     dispatch({ type: ACTIONS.DECREMENT });
   }
-  const onAddInput = (e) => {
+  const onAddInput = (e) => {import React, { useReducer } from 'react'
+
+import { counterReducer } from '../reducers/counterReducer';
+
+import '../styles/App.css';
+
+const App = () => {
+
+  const initialState = {
+
+  count:0
+
+  }
+
+const [state,dispatch] = useReducer(counterReducer,initialState)
+
+  return (
+
+    <div id="main">
+
+<button id='increment-btn' onClick={() => dispatch({type:'increment'})}>+</button>
+
+      <span id='counter'>{state.count}</span>
+
+      <button id='decrement-btn' onClick={() => dispatch({type:'decrement'})}>-</button>
+
+    </div>
+
+  )
+
+}
+
+export default App;
     dispatch({
       type: ACTIONS.SET_ADD_NUM,
       payload: e.target.value
